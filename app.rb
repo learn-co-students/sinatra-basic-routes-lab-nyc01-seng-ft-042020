9f199ffadf3bdcf618343d6eb310eb9b91663eda
Srishti Nath <srishti.nath@gmail.com>
2020-05-11 00:17:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Srishti"
+    end
+
+    get '/hometown' do
+        "My hometown is Bangalore, but grew up in East Brunswick"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is No Role Modelz"
+    end
 end
